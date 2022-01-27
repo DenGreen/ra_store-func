0aad2fc5292c3b0767590f164a3c10ec243b5967
Denis Malyshev <malyshev_denis@outlook.com>
2022-01-27 23:33:39 +0300

    commit 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Store func
+[![Build status](https://ci.appveyor.com/api/projects/status/tyh1hea66istdfqc?svg=true)](https://ci.appveyor.com/project/DenGreen/ra-store-func-m8mke)
+https://dengreen.github.io/ra_store-func/
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import ShopItemFunc from "./components/ShopItemFunc/shopItemFunc";
+
+function App() {
+  return (
+    <div className="container">
+    <div className="background-element">
+    </div>
+    <div className="highlight-window">
+      <div className='highlight-overlay'></div>
+    </div>
+    <div className="window">
+      <ShopItemFunc />
+    </div>
+  </div>
+  );
+}
+
+export default App;
